c514350a28a5513784f0ddd68d5295aee764ced9
ZT1996 <294238532@qq.com>
2016-08-18 13:19:31 +0800

    test

--- spec/dummy-spec.js
+++ spec/dummy-spec.js
@@ -2,91 +2,92 @@
 import request from 'supertest';
 import app from '../app/server';
 import {User} from '../app/db/schema';
+import finish from '../spec/finish';
 
-describe('test',() => {
-  beforeEach((done) => {
-    User.find().remove(finish(done));
-  });
-  it('用户名和密码正确',(done) => {
+describe('test', () => {
+
+  it('用户名和密码正确', (done) => {
     request(app)
-      .post('/')
-      .send({username:'nike1996',password:'123456'})
-      .expect({httpCode:201,text:'注册成功'},function (err, User) {
-        const saved = User[0];
-        expect(saved.username).toEqual('nike1996');
-        expect(saved.password).toEqual('123456');
+      .post('/api/users')
+      .send({userName: 'nike1996', password: '123456'})
+      .expect({httpCode: 201, massage: '注册成功'}, function (err, data) {
+        User.find(function (err, users) {
+          const saved = users[0];
+          expect(saved.userName).toEqual('nike1996');
+          expect(saved.password).toEqual('123456');
+          finish(done)(err);
+        });
+      })
+  });
+
+  it('用户名位数不正确', (done) => {
+    request(app)
+      .post('/api/users')
+      .send({userName: 'nike', password: '123456'})
+      .expect({httpCode: 400, message: '用户名只能是6-20位数字、字母组成，密码只能是6位数字'}, function (err, User) {
         finish(done)(err);
       })
   });
 
-  it('用户名位数不正确',(done) => {
+  it('密码位数不正确', (done) => {
     request(app)
-      .post('/')
-      .send({username:'nike',password:'123456'})
-      .expect({httpCode:400,text:'用户名或密码格式不正确'},function (err, User) {
+      .post('/api/users')
+      .send({userName: 'nike1996', password: '12345'})
+      .expect({httpCode: 400, message: '用户名只能是6-20位数字、字母组成，密码只能是6位数字'}, function (err, User) {
         finish(done)(err);
       })
   });
 
-  it('密码位数不正确',(done) => {
+  it('用户名和密码位数不正确', (done) => {
     request(app)
-      .post('/')
-      .send({username:'nike1996',password:'12345'})
-      .expect({httpCode:400,text:'用户名或密码格式不正确'},function (err, User) {
+      .post('/api/users')
+      .send({userName: 'nike', password: '12345'})
+      .expect({httpCode: 400, message: '用户名只能是6-20位数字、字母组成，密码只能是6位数字'}, function (err, User) {
         finish(done)(err);
       })
   });
 
-  it('用户名和密码位数不正确',(done) => {
+  it('用户名密码为空', (done) => {
     request(app)
-      .post('/')
-      .send({username:'nike',password:'12345'})
-      .expect({httpCode:400,text:'用户名或密码格式不正确'},function (err, User) {
+      .post('/api/users')
+      .send({userName: '', password: ''})
+      .expect({httpCode: 400, message: '用户名和密码不能为空'}, function (err, User) {
         finish(done)(err);
       })
   });
 
-  it('用户名密码为空',(done) => {
+  it('用户名有不能识别的字符', (done) => {
     request(app)
-      .post('/')
-      .send({username:'',password:''})
-      .expect({httpCode:400,text:'请输入用户名或密码'},function (err, User) {
+      .post('/api/users')
+      .send({userName: 'nike_+', password: '123456'})
+      .expect({httpCode: 400, message: '用户名只能是6-20位数字、字母组成，密码只能是6位数字'}, function (err, User) {
         finish(done)(err);
       })
   });
 
-  it('用户名有不能使别的字符',(done) => {
+  it('密码有不能识别的字符', (done) => {
     request(app)
-      .post('/')
-      .send({username:'nike_+',password:'123456'})
-      .expect({httpCode:400,text:'用户名只能由6-20位数资或字母组成'},function (err, User) {
+      .post('/api/users')
+      .send({userName: 'nike1996', password: '1234_+'})
+      .expect({httpCode: 400, message: '用户名只能是6-20位数字、字母组成，密码只能是6位数字'}, function (err, User) {
         finish(done)(err);
       })
   });
 
-  it('密码有不能识别的字符',(done) => {
+  it('用户名密码有不能识别的字符', (done) => {
     request(app)
-      .post('/')
-      .send({username:'nike1996',password:'1234_+'})
-      .expect({httpCode:400,text:'密码只能由6位数字组成'},function (err, User) {
+      .post('/api/users')
+      .send({userName: 'nike_+', password: '123456'})
+      .expect({httpCode: 400, message: '用户名只能是6-20位数字、字母组成，密码只能是6位数字'}, function (err, User) {
         finish(done)(err);
       })
   });
 
-  it('用户名密码有不能识别的字符',(done) => {
+  it('用户名已存在', (done) => {
     request(app)
-      .post('/')
-      .send({username:'nike_+',password:'123456'})
-      .expect({httpCode:400,text:'用户名只能由6-20位数资或字母组成,密码只能由6位数字'},function (err, User) {
-        finish(done)(err);
-      })
-  });
-
-  it('用户名已存在',(done) =>{
-    request(app)
-      .post('/')
-      .send({username:'nike',password:'123456'})
-      .expect({httpCode:409,text:'该用户已存在'},function (err, User) {
+      .post('/api/users')
+      .send({userName: 'nike1996', password: '123456'})
+      .expect({httpCode: 409, message: '该用户已存在'}, function (err, User) {
         finish(done)(err);
       })
   });
