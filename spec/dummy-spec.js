108adbec57da0d2598618be8ba91ba53178d0854
ZT1996 <294238532@qq.com>
2016-08-18 20:30:20 +0800

    spec

--- spec/dummy-spec.js
+++ spec/dummy-spec.js
@@ -15,91 +15,116 @@ describe('test true', () => {
   });
 
   it('用户名和密码正确', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike1996', password: '123456'})
-      .expect(201, '注册成功', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike1996', password: '123456'})
+        .expect(201, cb, '注册成功'),
+    ], finish(done));
   });
-
 });
 
 describe('unit count', () => {
   it('用户名位数不正确', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike', password: '123456'})
-      .expect(400, '用户名只能是6-20位数字、字母组成', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike', password: '123456'})
+        .expect(400, cb, '用户名只能是6-20位数字、字母组成'),
+    ], finish(done));
+
   });
 
   it('密码位数不正确', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike1996', password: '12345'})
-      .expect(400, '密码只能是6位数字', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike1996', password: '12345'})
+        .expect(400, cb, '密码只能是6位数字'),
+    ], finish(done));
   });
 
   it('用户名和密码位数不正确', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike', password: '12345'})
-      .expect(400, '用户名只能是6-20位数字、字母组成', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike', password: '12345'})
+        .expect(400, cb, '用户名只能是6-20位数字、字母组成'),
+    ], finish(done));
   });
 });
 
 describe('unit string number', () => {
   it('用户名有不能识别的字符', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike_+', password: '123456'})
-      .expect(400, '用户名只能是6-20位数字、字母组成', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike_+', password: '123456'})
+        .expect(400, cb, '用户名只能是6-20位数字、字母组成'),
+    ], finish(done));
   });
 
   it('密码有不能识别的字符', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike1996', password: '1234_+'})
-      .expect(400, '密码只能是6位数字', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike1996', password: '1234_+'})
+        .expect(400, cb, '密码只能是6位数字'),
+    ], finish(done));
   });
 
   it('用户名密码有不能识别的字符', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike_+', password: '123456'})
-      .expect(400, '用户名只能是6-20位数字、字母组成', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike_+', password: '123456'})
+        .expect(400, cb, '用户名只能是6-20位数字、字母组成'),
+    ], finish(done));
   });
 
 });
 
 describe('unit undefined', () => {
+
   it('用户名为空', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: '', password: '123456'})
-      .expect(400, '用户名和密码不能为空', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: '', password: '123456'})
+        .expect(400, cb, '用户名和密码不能为空'),
+    ], finish(done));
+
   });
 
   it('密码为空', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike0000', password: ''})
-      .expect(400, '用户名和密码不能为空', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike0000', password: ''})
+        .expect(400, cb, '用户名和密码不能为空'),
+    ], finish(done));
+
   });
 
   it('用户名密码为空', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: '', password: ''})
-      .expect(400, '用户名和密码不能为空', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: '', password: ''})
+        .expect(400, cb, '用户名和密码不能为空'),
+    ], finish(done));
   });
 });
 
 describe('test already exist', () => {
-
   it('用户名已存在', (done) => {
-    request(app)
-      .post('/api/users')
-      .send({userName: 'nike1996', password: '123456'})
-      .expect(409, '该用户已存在', finish(done));
+    async.waterfall([
+      (cb) => request(app)
+        .post('/api/users')
+        .send({userName: 'nike1996', password: '123456'})
+        .expect(409, cb, '该用户已存在'),
+    ], finish(done));
+
   });
   afterEach((done) => {
     db.close(finish(done));
