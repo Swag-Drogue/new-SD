6b2431fab5c7c2be1d87b062cd645dc3067cb95d
maruiqi <maruiqiqiqiqiqi@163.com>
2016-08-20 14:11:58 +0800

    login-page

--- public/entry.jsx
+++ public/entry.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Register from './register.jsx';
+import {Router, Route, hashHistory} from 'react-router';
+import Login from './login.jsx'
+
 require('jquery');
 require('bootstrap-webpack');
 require('./style/register.css');
+require('./style/login.css');
 
-ReactDOM.render(
-  <Register/>,
-  document.getElementById('content')
-);
 
+ReactDOM.render((
+  <Router history={hashHistory}>
+    <Route path='/' component={Login}/>
+    <Route path='/register' component={Register}/>
+    <Route path='/login' component={Login}/>
+  </Router>
+), document.getElementById('content'));
 if (module.hot) {
   module.hot.accept();
 }
