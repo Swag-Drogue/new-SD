51c5a842627716aedb85d11c5ebbfec0a325f8ba
Liuhaiyan6 <2391348695@qq.com>
2016-08-22 10:18:15 +0800

    modify-login

--- app/api/sessions.js
+++ app/api/sessions.js
@@ -1,22 +1,21 @@
 'use strict';
 import express from 'express';
 import {User} from '../db/schema';
-import {isEmpty, validUserName, validPassword} from '../../shared/register-validation';
+import {isEmpty} from '../../shared/register-validation';
 
 const router = express.Router();
 router.post('/', function (req, res, next) {
   const {userName, password} = req.body;
   const userData = {userName, password};
-  console.log(userData);
   if (isEmpty(userData)) {
     res.status(400).send('用户名或密码不能为空');
   } else {
-    User.findOne({userName: userData.userName}, function (err, user) {
+    User.findOne({userName}, function (err, user) {
       if (err) return next(err);
       if (user === null) {
         return res.status(401).send('用户名不存在');
       }
-      User.findOne({userName: userData.userName, password: userData.password}, function (err, user) {
+      User.findOne(userData, function (err, user) {
         if (err) return next(err);
         if (user === null) {
           return res.status(401).send('密码错误');
--- spec/login-spec.js
+++ spec/login-spec.js
@@ -3,13 +3,13 @@ import request from 'supertest';
 import app from '../app/server';
 import finish from '../spec/finish';
 import db from '../app/db/db';
-import async from 'async';
 
 describe('测试', () => {
   beforeEach((done) => {
-    async.series([
-      (cb) => db.connect('test', cb)
-    ], finish(done));
+    db.connect('test',finish(done));
+  });
+  afterEach((done) => {
+    db.close(finish(done));
   });
   describe('测试为真', () => {
     it('登录成功', (done) => {
@@ -19,7 +19,7 @@ describe('测试', () => {
         .expect(201, '登录成功', finish(done));
     });
   });
-  describe('测试用户名或密码不能为空', () => {
+  describe('不能成功登录', () => {
     it('密码为空', (done) => {
       request(app)
         .post('/api/sessions')
@@ -33,21 +33,18 @@ describe('测试', () => {
         .expect(400, '用户名或密码不能为空', finish(done));
     });
   });
-  describe('测试用户名或密码不正确', () => {
-    it('密码不正确', (done) => {
-      request(app)
-        .post('/api/sessions')
-        .send({userName: 'nike1996', password: '1419234'})
-        .expect(401, '密码错误', finish(done));
-    });
+  describe('提交数据格式不正确', () => {
     it('用户名不正确', (done) => {
       request(app)
         .post('/api/sessions')
-        .send({userName: 'nike1997', password: '1419234'})
+        .send({userName: 'nike1997', password: '123456'})
         .expect(401, '用户名不存在', finish(done));
     });
-  });
-  afterEach((done) => {
-    db.close(finish(done));
+    it('密码不正确', (done) => {
+      request(app)
+        .post('/api/sessions')
+        .send({userName: 'nike1996', password: '123457'})
+        .expect(401, '密码错误', finish(done));
+    });
   });
 });
