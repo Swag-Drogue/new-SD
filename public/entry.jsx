c41f624367ddcfcadf903e5e54fe00b8da3ee11e
maruiqi <maruiqiqiqiqiqi@163.com>
2016-08-22 09:34:40 +0800

    login-page2

--- public/entry.jsx
+++ public/entry.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Register from './register.jsx';
 import {Router, Route, hashHistory} from 'react-router';
 import Login from './login.jsx'
-import 'jquery'
+import 'jquery';
 import 'bootstrap-webpack';
 import './style/register.css';
 import './style/login.css';
