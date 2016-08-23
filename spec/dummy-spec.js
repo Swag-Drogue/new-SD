eee5f5f9a1da1c10e8ae3672bcf7d346e415f473
ZT1996 <294238532@qq.com>
2016-08-23 17:03:01 +0800

    init

--- spec/dummy-spec.js
+++ spec/dummy-spec.js
@@ -5,7 +5,7 @@ import finish from '../spec/finish';
 import db from '../app/db/db';
 import {User} from '../app/db/schema';
 import async from 'async';
-describe('test',() => {
+describe('test', () => {
   describe('test true', () => {
     beforeEach((done) => {
       async.series([
@@ -19,7 +19,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike1996', password: '123456'})
-          .expect(201, cb,'注册成功'),
+          .expect(201, cb, '注册成功'),
       ], finish(done));
     });
   });
@@ -30,7 +30,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike', password: '123456'})
-          .expect(400, cb,'用户名只能是6-20位数字、字母组成'),
+          .expect(400, cb, '用户名只能是6-20位数字、字母组成'),
       ], finish(done));
 
     });
@@ -40,7 +40,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike1996', password: '12345'})
-          .expect(400, cb,'密码只能是6位数字'),
+          .expect(400, cb, '密码只能是6位数字'),
       ], finish(done));
     });
 
@@ -49,7 +49,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike', password: '12345'})
-          .expect(400, cb,'用户名只能是6-20位数字、字母组成'),
+          .expect(400, cb, '用户名只能是6-20位数字、字母组成'),
       ], finish(done));
     });
   });
@@ -60,7 +60,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike_+', password: '123456'})
-          .expect(400, cb,'用户名只能是6-20位数字、字母组成'),
+          .expect(400, cb, '用户名只能是6-20位数字、字母组成'),
       ], finish(done));
     });
 
@@ -69,7 +69,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike1996', password: '1234_+'})
-          .expect(400, cb,'密码只能是6位数字'),
+          .expect(400, cb, '密码只能是6位数字'),
       ], finish(done));
     });
 
@@ -78,7 +78,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike_+', password: '123456'})
-          .expect(400, cb,'用户名只能是6-20位数字、字母组成'),
+          .expect(400, cb, '用户名只能是6-20位数字、字母组成'),
       ], finish(done));
     });
   });
@@ -90,7 +90,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: '', password: '123456'})
-          .expect(400, cb,'用户名和密码不能为空'),
+          .expect(400, cb, '用户名和密码不能为空'),
       ], finish(done));
 
     });
@@ -100,7 +100,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike0000', password: ''})
-          .expect(400, cb,'用户名和密码不能为空'),
+          .expect(400, cb, '用户名和密码不能为空'),
       ], finish(done));
 
     });
@@ -110,7 +110,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: '', password: ''})
-          .expect(400, cb,'用户名和密码不能为空'),
+          .expect(400, cb, '用户名和密码不能为空'),
       ], finish(done));
     });
   });
@@ -121,7 +121,7 @@ describe('test',() => {
         (cb) => request(app)
           .post('/api/users')
           .send({userName: 'nike1996', password: '123456'})
-          .expect(409, cb,'该用户已存在'),
+          .expect(409, cb, '该用户已存在'),
       ], finish(done));
 
     });
