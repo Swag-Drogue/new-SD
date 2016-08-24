5a5c00e2abcbbae9e1e1a5e685cba69df2b09374
ZT1996 <294238532@qq.com>
2016-08-24 10:01:31 +0800

    init

--- app/api/api.js
+++ app/api/api.js
@@ -1,13 +1,11 @@
 import express from 'express';
 import userApi from './users';
 import sessionsApi from './sessions';
-import articleApi from './article';
 import uploadedImagesApi from './uploaded-images';
 const router = express.Router();
 
 router.use('/users', userApi);
 router.use('/sessions', sessionsApi);
-router.use('/article', articleApi);
 router.use('/uploadedImages', uploadedImagesApi);
 
 export default router;
--- public/component/editor.jsx
+++ public/component/editor.jsx
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
-import {Link, hashHistory} from 'react-router';
-import request from 'superagent';
-import App from './app.jsx';
-import UploadForm from './upload.jsx';
+import request from 'superagent'
+import _ from 'lodash';
 
 export default class Editor extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
-      content: ''
+      selectedImage: null,
+      article: '',
+      author:'',
+      uploadedImages: []
     }
   }
 
@@ -29,59 +30,86 @@ export default class Editor extends Component {
   }
 
   render() {
-    return (
-      <div className="container-fluid wrapper">
-        <div>
-          <App/>
+    return <div className="wrapper container-fluid">
+      <form onSubmit={this._onImgUpload.bind(this)}>
+        <div className="form-group">
+          <label htmlFor="images">图片</label>
+          <div>
+            {this.state.uploadedImages.map((i, index) => <img key={index} src={i}/>)}
+            {/*加上key对应uploadedmagdes数组里的图片*/}
+          </div>
+          <input type="file" id="images" accept=".jpg,.jpeg,.png,.gif"
+                 onChange={(e)=>this._handleImageChange(e)}/>
+          <input type="submit" value='上传图片' onSubmit={this._onImgUpload.bind(this)}/>
         </div>
-        <form>
-          <div className="form-group">
-            <label htmlFor="title"><h3>标题：</h3></label>
-            <input type="text" className="form-control" id="title" placeholder="请在此输入标题"
-                   onChange={this._onTitleChange.bind(this)} required/>
-          </div>
-          <label htmlFor="text-body"><h3>内容：</h3>
-          </label>
-          <div id="text-body">
-            <div className="upload">
-              <UploadForm/>
-            </div>
-            <div className="p-body" contentEditable="true" placeholder="在此输入正文"
-                 onChange={this._onContentChange.bind(this)}></div>
-          </div>
-          <button className="submit" onClick={this._onSubmit.bind(this)}>
-            <Link to="/share">发布</Link>
-          </button>
-        </form>
-      </div>
-    )
+      </form>
+      <form onSubmit={this._onSubmit.bind(this)}>
+        <div className="form-group">
+          <label htmlFor="title">标题</label>
+          <input type="text" className="form-control" id="title" placeholder="请输入标题"
+                 value={this.state.title}
+                 onChange={this._onTitleChange.bind(this)}
+                 required="true"/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="name">文本框</label>
+          <textarea className="form-control" rows="3"
+                    value={this.state.article}
+                    onChange={this._onArticleChange.bind(this)}/>
+        </div>
+        <button className="btn btn-default btn-right" type="submit">提交</button>
+      </form>
+    </div>
   }
 
   _onTitleChange(event) {
     this.setState({
       title: event.target.value
-    })
+    });
   }
 
-  _onContentChange(event) {
+  _handleImageChange(event) {
+    const file = event.target.files[0];
     this.setState({
-      content: event.target.value
-    })
+      selectedImage: file
+    });
   }
 
-  _onSubmit() {
-    request
-      .post('')
+  _onImgUpload(event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('image', this.state.selectedImage);
+
+    request.post('/api/uploaded-images')
+      .send(formData)
+      .end((err, res) => {
+        if (err) return alert('uploading failed!');
+        const uploadedImagePath = res.text;
+        this.setState({
+          uploadedImages: _.concat(this.state.uploadedImages, uploadedImagePath)
+        });
+      })
+  }
+
+  _onArticleChange(event) {
+    this.setState({
+      article: event.target.value
+    });
+  }
+
+  _onSubmit(event) {
+    event.preventDefault();
+    request.post('/api/articles')
       .send({
         title: this.state.title,
-        content: this.state.content
+        article: this.state.article,
+        images: this.state.uploadedImages
       })
-      .end((err, res) => {
+      .end((err, res)=> {
         if (err) return alert(res.text);
         alert(res.text);
-      })
+      });
   }
 }
-
-
-
