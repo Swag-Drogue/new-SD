97bfc38a07634ad40c4baf45922e8bd8fc66f386
Yvette <libra1014tyb@163.com>
2016-08-31 19:58:24 +0800

    change input to button

--- public/component/editor.jsx
+++ public/component/editor.jsx
@@ -49,7 +49,7 @@ export default class Editor extends Component {
             </div>
             <input type="file" id="images" className="btn btn-default" required="true" accept=".jpg,.jpeg,.png,.gif"
                    onChange={(e)=>this._handleImageChange(e)}/>
-            <input type="edit-button" className="btn btn-default" value='上传图片' onClick={this._onImgUpload.bind(this)}/>
+            <button type="edit-button" className="btn btn-default" onClick={this._onImgUpload.bind(this)}>上传图片</button>
           </div>
         </div>
         <div className="form-group">
