0306603a3d6b0e852d5001b3f386e410ff159762
Yvette <libra1014tyb@163.com>
2016-08-31 15:43:01 +0800

    mongod

--- app/db/db.js
+++ app/db/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 module.exports = {
   connect: function (mode, callback) {
-    let url = process.env.MONGOLAB_URI || 'mongodb://localhost/new-SD';
+    let url = process.env.PROD_MONGODB || 'mongodb://localhost/new-SD';
     if (mode === 'test') {
       url = 'mongodb://localhost/new-SD-test';
     }
